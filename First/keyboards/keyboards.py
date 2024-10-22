71cac2162b9d5f5abf2c3c2a25201761a0c5171a
osinkinVS <vosinkind@gmail.com>
2024-10-22 21:49:31 +0300

    Update keyboards.py

--- First/keyboards/keyboards.py
+++ First/keyboards/keyboards.py
@@ -4,10 +4,11 @@ button1 = types.KeyboardButton(text='/start')
 button2 = types.KeyboardButton(text='Информация')
 button3 = types.KeyboardButton(text='покажи лису')
 button4 = types.KeyboardButton(text='/stop')
+button5 = types.KeyboardButton(text='/prof')
 
 keyboard1 = [
     [button1, button2],
-    [button3, button4],
+    [button3, button4, button5],
 ]
 
 keyboard2 = [
